98d5ac5a1724d6fd79314c08c608cdab17307abc
Victor Sandoval-Luna <stb.4421@gmail.com>
2019-11-25 21:32:25 -0600

    PortHandler class updates. License update in PortHandler files

--- c++/include/herkulex_sdk/port_handler.h
+++ c++/include/herkulex_sdk/port_handler.h
@@ -1,23 +1,43 @@
-/*******************************************************************************
-* Copyright 2018 Robótica de la Mixteca
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
-
-////////////////////////////////////////////////////////
-/// @file Direct port handler through USB-TTL converter
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Robótica de la Mixteca
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Universidad Tecnológica de la Mixteca nor
+ *     the names of its contributors may be used to endorse or promote
+ *     products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+ 
+/////////////////////////////////////////////////////////////////////////////////////////
+/// @file PortHandler class definition of the MSV-01 rescue robot.
 /// @author Victor Esteban Sandoval-Luna
-////////////////////////////////////////////////////////
+///
+/////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef HERKULEX_SDK_PORTHANDLER_H_
 #define HERKULEX_SDK_PORTHANDLER_H_
@@ -37,36 +57,23 @@ namespace herkulex
 class PortHandler
 {
   private:
-    // attributes
-    char*   port_name;
-    int     socket_fd;
+    // Attributes
+    char*   port_name_;
+    int     socket_fd_;
     int     baudrate_;
 
-    double  packet_start_time_;
-    double  packet_timeout_;
-    double  tx_time_per_byte;
-
-    bool using_;
-
-    // methods
-    double getCurrentTime();
-    double getTimeSinceStart();
+    // Methods
 
     // Sets the attributes of the serial interface
-    int setInterfaceAttribs (int fd, int baudrate, int parity);
-    // Sets if the port blocks or not
-    void setBlocking (int fd, int block);
+    int setInterfaceAttribs (const int& fd, const int& baudrate, const int& parity);
 
-    int remapBaudRate(const int baudrate);
+    // Default Baudrate
+		const int DEFAULT_BAUDRATE = 115200;
 
   public:
-    // Default HerkuleX Baudrate
-    const int DEFAULT_BAUDRATE = 115200;
-
     // Constructors
     PortHandler ();
-
-    PortHandler (const char *portname);
+    PortHandler (char* portname, const int& baudrate);
 
     virtual ~PortHandler() { }
 
@@ -80,26 +87,22 @@ class PortHandler
     void clearPort();
 
     // Sets the name of the port
-    void setPortName(const char* port_name);
+    void setPortName(char* port_name);
 
     // Gets the name of the port
-    char *getPortName ();
+    char* getPortName ();
 
     // Sets the baudrate of the port
-    int setBaudRate(const int baudrate);
-
-    // Gets the baudrate of the port
-    int getBaudRate();
+    int setBaudRate(const int& baudrate);
 
     // Gets the number of bits available for reading from the port buffer
-    int getBytesAvailable();
+    int getBytesAvailable ();
 
     // Reads port
-    int readPort(uint8_t *packet, int length);
+    int readPort(uint8_t *packet, const int& length);
 
     // Writes to port
-    int writePort(uint8_t *packet, int length);
-
+    int writePort(uint8_t *packet, const int& length);
 };
 
 }
--- c++/src/herkulex_sdk/port_handler.cpp
+++ c++/src/herkulex_sdk/port_handler.cpp
@@ -34,9 +34,10 @@
  *********************************************************************/
  
 /////////////////////////////////////////////////////////////////////////////////////////
-/// @file Port handler
-/// @author Victor Esteban Sandoval-Luna
+/// @file PortHandler class implementation. This class allow serial port read/write
+/// non-canonical control. It is intended to be used with FTDI USB-to-TTL converters.
 ///
+/// @author Victor Esteban Sandoval-Luna
 /////////////////////////////////////////////////////////////////////////////////////////
 
 #include "../../include/herkulex_sdk/port_handler.h"
@@ -44,19 +45,22 @@
 using namespace herkulex;
 
 // Constructors
-PortHandler::PortHandler () {
+PortHandler::PortHandler ()
+{
   port_name = (char*)"/dev/ttyUSB0";
   socket_fd = -1;
-  setBaudRate(115200);
+  setBaudRate(DEFAULT_BAUDRATE);
 }
 
-PortHandler::PortHandler (const char *portname) {
-  port_name = (char*)portname;
+PortHandler::PortHandler (char* const portname, const int& baudrate)
+{
+  port_name = portname;
   socket_fd = -1;
-  setBaudRate(115200);
+  setBaudRate(baudrate);
 }
 
-bool PortHandler::openPort () {
+bool PortHandler::openPort ()
+{
   socket_fd = open (port_name, O_RDWR | O_NOCTTY | O_SYNC);
 
   if (socket_fd < 0) {
@@ -65,32 +69,35 @@ bool PortHandler::openPort () {
   }
 
   setInterfaceAttribs(socket_fd, baudrate_, 0);
-  setBlocking(socket_fd, 0);
   tcflush(socket_fd, TCIFLUSH);
 
   return true;
 }
 
-void PortHandler::closePort () {
-  if(socket_fd != -1)
-    close(socket_fd);
-  socket_fd = -1;
+void PortHandler::closePort ()
+{
+  if(socket_fd_ != -1) {close(socket_fd_);}
+  socket_fd_ = -1;
 }
 
-void PortHandler::clearPort () {
+void PortHandler::clearPort ()
+{
   tcflush(socket_fd, TCIFLUSH);
 }
 
-void PortHandler::setPortName (const char* portname) {
+void PortHandler::setPortName (char* const portname)
+{
   port_name = (char*)portname;
 }
 
-char* PortHandler::getPortName () {
-  return port_name;
+char* PortHandler::getPortName ()
+{
+  return port_name_;
 }
 
-int PortHandler::setBaudRate (const int baudrate) {
-  // Considering both Hovis HerkuleX servos limits and TTL hardware limits (up to 500000 bps)
+int PortHandler::setBaudRate (const int& baudrate)
+{
+  // Considering both Hovis HerkuleX servos limits and USB-to-TTL hardware limits (up to 500000 bps)
   int br = -1;
 
   switch (baudrate) {
@@ -121,37 +128,69 @@ int PortHandler::setBaudRate (const int baudrate) {
   }
 
   if (br == -1) {
-    std::cout << "Error setting baudrate: Invalid baudrate." << std::endl;
-    return br;
+    std::cout << "Error setting baudrate (Invalid baudrate): Default baudrate 115200 bps." << std::endl;
   }
 
-  return baudrate;
+  return br;
 }
 
-int PortHandler::getBaudRate ()
+int PortHandler::getBytesAvailable ()
 {
-  return remapBaudRate(baudrate_);
-}
-
-int PortHandler::getBytesAvailable () {
   int bytes_available;
   ioctl(socket_fd, FIONREAD, &bytes_available);
   return bytes_available;
 }
 
-int PortHandler::readPort (uint8_t *packet, int length) {
+int PortHandler::readPort (uint8_t* const packet, const int& length)
+{
   return read(socket_fd, packet, length);
 }
 
-int PortHandler::writePort (uint8_t *packet, int length) {
+int PortHandler::writePort (uint8_t* const packet, const int& length)
+{
   return write(socket_fd, packet, length);
 }
 
-int PortHandler::setInterfaceAttribs (int fd, int baudrate, int parity) {
+int PortHandler::setInterfaceAttribs (const int& fd, const int& baudrate, const int& parity)
+{
   struct termios tty;
-  memset(&tty, 0, sizeof tty);
 
-  if(!isatty(fd)) {
+  if (tcgetattr(fd, &tty) < 0) {
+    std::cout << "Error getting term attributes of " << port_name_ << ": " << std::strerror(errno) <<  std::endl;;
+    return -1;
+  }
+
+  cfsetospeed(&tty, baudrate);
+  cfsetispeed(&tty, baudrate);
+
+  // cflag
+  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;		// 8-bit chars
+  tty.c_cflag |= (CLOCAL | CREAD);	// ignore modem controls, enable reading
+  tty.c_cflag &= ~(PARENB | PARODD);	// shut off parity
+  tty.c_cflag |= parity;
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag &= ~CRTSCTS;
+
+  // iflag
+  tty.c_iflag &= ~IGNBRK;		// disable break processing
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY);		// shut off xon/xoff ctrl
+
+  // lflag
+  tty.c_lflag = 0;	// no signaling chars, no echo,non-canonical processing
+
+  // oflag
+  tty.c_oflag = 0;	// no remapping, no delays
+
+  // cc
+  tty.c_cc[VMIN]  = 0;	// read doesn't block
+  tty.c_cc[VTIME] = 5;	// 0.5 seconds read timeout
+
+  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+    std::cout << "Error " << std::strerror(errno) << " from tcsetattr.\n";
+    return -1;
+  }
+
+  if (!isatty(fd)) {
     std::cout << "Error " << std::strerror(errno) << " from isatty.\n";
     return -1;
   }
@@ -160,70 +199,6 @@ int PortHandler::setInterfaceAttribs (int fd, int baudrate, int parity) {
     std::cout << "Error " << std::strerror(errno) << " from tcgetattr.\n";
     return -1;
   }
-
-  cfsetospeed(&tty, baudrate);
-  cfsetispeed(&tty, baudrate);
-
-  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-  // disable IGNBRK for mismatched speed tests; otherwise receive break
-  // as \000 chars
-  tty.c_iflag &= ~IGNBRK;         // disable break processing
-  tty.c_lflag = 0;                // no signaling chars, no echo,
-                                  // no canonical processing
-  tty.c_oflag = 0;                // no remapping, no delays
-  tty.c_cc[VMIN]  = 0;            // read doesn't block
-  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-
-  tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                  // enable reading
-  tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-  tty.c_cflag |= parity;
-  tty.c_cflag &= ~CSTOPB;
-  tty.c_cflag &= ~CRTSCTS;
-
-  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-    std::cout << "Error " << std::strerror(errno) << " from tcsetattr.\n";
-    return -1;
-  }
-
   return 0;
 }
 
-void PortHandler::setBlocking (int fd, int block) {
-  struct termios tty;
-  memset(&tty, 0, sizeof tty);
-
-  if (tcgetattr(fd, &tty) != 0) {
-    std::cout << "Error " << std::strerror(errno) << " from tggetattr.\n";
-    return;
-  }
-
-  tty.c_cc[VMIN]  = block ? 1 : 0;
-  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-  if (tcsetattr(fd, TCSANOW, &tty) != 0)
-    std::cout << "Error " << std::strerror(errno) << " setting termios attributes.\n";
-}
-
-int PortHandler::remapBaudRate (const int baudrate) {
-  switch (baudrate) {
-    case B9600:
-      return 9600;
-    case B19200:
-      return 19200;
-    case B38400:
-      return 38400;
-    case B57600:
-      return 57600;
-    case B115200:
-      return 115200;
-    case B230400:
-      return 230400;
-    case B460800:
-      return 460800;
-    default:
-      return -1;
-  }
-}
